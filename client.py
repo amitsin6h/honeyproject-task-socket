2b321ba757c7e520ec2de6b2028810f243210945
Amit singh <amitsin6h@gmail.com>
2017-04-23 13:59:57 +0530

    read config from file and generate error on logs file

--- client.py
+++ client.py
@@ -1,19 +1,36 @@
 import socket
- 
-def client():
-        host = socket.gethostname()
-        port =  2222
+import time
 
-        #creating a socket object 
-        server_socket = socket.socket()
-       
-       #connecting to host on port
-        server_socket.connect((host,port))
+
+def client():
+        localtime = time.asctime(time.localtime(time.time()))
+    	try:
+            logs = open("logs.txt","a")
+            config_file = open("config.txt","r")
+            data = {}
+            for file_data in config_file:
+                k, v = file_data.strip().split('=')
+                data[k.strip()] = v.strip()
+            port_no = int(data['port'])
+        except IOError:
+            logs.write("%s IOError: can\'t find file or read data \n" % ([localtime]))
+            logs.close()
+        except KeyError:
+            logs.write("%s KeyError: key not found on dictionary \n" % ([localtime]))
+        else:
+            host = socket.gethostname()
+            port = port_no   
+
+            #creating a socket object 
+            server_socket = socket.socket()
+
+            #connecting to host on port
+            server_socket.connect((host,port))
         
-        #get raw_input
-        message = raw_input("Enter Your Message:")
-        server_socket.send(message.encode())
+            #get raw_input
+            message = raw_input("Enter Your Message:")
+            server_socket.send(message.encode())
                  
-        server_socket.close()
+            server_socket.close()
  
 client()
--- server.py
+++ server.py
@@ -1,25 +1,42 @@
 import socket
- 
-def server():
-    host = socket.gethostname()
-    port = 2222
-    
-    #creating socket object
-    server_socket = socket.socket()
-   
-   #bindig host and port
-    server_socket.bind((host,port))
-    
-    #response up to 3 request    
-    server_socket.listen(3)
-   
-    client_socket, addr = server_socket.accept()
-    print ("Got connection from: " + str(addr))
+import time
 
-    data = client_socket.recv(1024).decode()
-    print ("Received message: " + str(data))
+
+def server():
+    localtime = time.asctime(time.localtime(time.time()))
+    try:
+        logs = open("logs.txt","a")
+        config_file = open("config.txt","r")
+        data = {}
+        for file_data in config_file:
+            k, v = file_data.strip().split('=')
+            data[k.strip()] = v.strip()
+        port_no = int(data['port'])
+    except IOError:
+        logs.write("%s IOError: can\'t find file or read data \n" % ([localtime]))
+        logs.close()
+    except KeyError:
+        logs.write("%s KeyError: key not found on dictionary \n" % ([localtime]))
+    else:
+        host = socket.gethostname()
+        port = port_no   
+    
+        #creating socket object
+        server_socket = socket.socket()
+   
+        #bindig host and port
+        server_socket.bind((host,port))
+    
+        #response up to 3 request    
+        server_socket.listen(3)
+   
+        client_socket, addr = server_socket.accept()
+        print ("Got connection from: " + str(addr))
+
+        data = client_socket.recv(1024).decode()
+        print ("Received message: " + str(data))
              
              
-    client_socket.close()
+        client_socket.close()
      
 server()
